cdf3b20ff346914ea333ba5832d504ab60642de9
Lee <leescoding@gmail.com>
2018-05-06 11:10:43 -0600

    Added wget install, gnome desktop install and hostname option

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -2,7 +2,7 @@
 
 # file names & paths
 tmp="$HOME"  # destination folder to store the final iso file
-hostname="ubuntu"
+read -ep " please enter your preferred username: " hostname
 currentuser="$( whoami)"
 
 # define spinner function for slow tasks
@@ -205,6 +205,9 @@ sed -i -r 's/timeout\s+[0-9]+/timeout 1/g' $tmp/iso_new/isolinux/isolinux.cfg
 if [[ $ub1604 == "yes" ]]; then
    late_command="apt-install wget; in-target wget --no-check-certificate -O /home/$username/start.sh https://github.com/netson/ubuntu-unattended/raw/master/start.sh ;\
      in-target chmod +x /home/$username/start.sh ;"
+else
+   # Update apt repo, install wget and gnome desktop
+   late_command="apt-get update; apt install wget -y; apt install ubuntu-gnome-desktop -y;"
 fi
 # else 
 #   late_command="chroot /target wget -O /home/$username/start.sh https://github.com/netson/ubuntu-unattended/raw/master/start.sh ;\
